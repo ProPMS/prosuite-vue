900cbe80a4db8b91eb1fb773516b0f3b49f099bd
Prince Radebe <princeradebe@gmail.com>
2023-07-26 00:55:20 +0200

    Fix Tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,56 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+
   theme: {
-    extend: {},
+    fontSize: {
+      '9xl': '8rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans'],
+      },
+      colors: {
+        primary: {
+          50: '#84BE35',
+        },
+        secondary: {
+          50: '#B5B5B6'
+        },
+        blue: {
+          50: '#54B9ED',
+          100: '#2E6CA8',
+          200: '#348ADD'
+        }
+      },
+      lineHeight: {
+        'extra-loose': '0.9',
+      },
+    },
+  },
+
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+    },
   },
   plugins: [],
 }
